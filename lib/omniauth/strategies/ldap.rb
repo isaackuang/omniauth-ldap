7f427e9f24478949e859c2300dc18d92b9b5ef9f
isaackuang <isaac.kuang@gmail.com>
2013-10-28 09:50:25 +0800

    fix blank password bug.

--- lib/omniauth/strategies/ldap.rb
+++ lib/omniauth/strategies/ldap.rb
@@ -3,8 +3,7 @@ require 'omniauth'
 module OmniAuth
   module Strategies
     class LDAP
-      class MissingCredentialsError < StandardError; end
-      include OmniAuth::Strategy
+     include OmniAuth::Strategy
       @@config = {
         'name' => 'cn',
         'first_name' => 'givenName',
@@ -38,7 +37,7 @@ module OmniAuth
       def callback_phase
         @adaptor = OmniAuth::LDAP::Adaptor.new @options
 
-        raise MissingCredentialsError.new("Missing login credentials") if request['username'].nil? || request['password'].nil?
+        return fail!(:missing_credentials) if missing_credentials?
         begin
           @ldap_user_info = @adaptor.bind_as(:filter => filter(@adaptor), :size => 1, :password => request['password'])
           return fail!(:invalid_credentials) if !@ldap_user_info
@@ -89,6 +88,10 @@ module OmniAuth
         end
         user
       end
+      protected
+      def missing_credentials?
+        request['username'].nil? or request['username'].empty? or request['password'].nil? or request['password'].empty?
+      end # missing_credentials?
     end
   end
 end
